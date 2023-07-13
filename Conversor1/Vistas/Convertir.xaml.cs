06cdd1b0e2d4ca7aa0f606b92a36ec26505fbb6f
Mhela4444 <melanytipan100@gmail.com>
2023-07-13 09:19:25 -0500

    Conversor

---
+++ Conversor1/Vistas/Convertir.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor1.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        double cm;
+        double m;
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+        private void calcular()
+        {
+            cm =Convert.ToDouble(txtcm.Text);
+            m = cm / 100;
+            lblres.Text = m.ToString() + " m";
+        }
+        private void Validar()
+        {
+            if (!string.IsNullOrEmpty(txtcm.Text))
+            {
+                calcular();
+
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese una cantidad", "OK");
+            }
+    }
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+            Validar();
+        }
+
+        private void btncalcular_Clicked_1(object sender, EventArgs e)
+        {
+            calcular();
+        }
+
+        private void btnvolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+
+        }
+    }
+}
+
